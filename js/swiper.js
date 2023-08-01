940af4f7db494324134c778696e3f38e8eee970e
Roman Selitskiy <roma.selitskiy@gmail.com>
2023-08-01 21:18:19 +0300

    about

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,4 @@
-const membershipSwiper = new Swiper('.membership__swiper', {
+const membershipSwiper = new Swiper(".membership__swiper", {
   slidesPerView: 1.2,
   spaceBetween: 12,
   initialSlide: 1,
@@ -6,13 +6,13 @@ const membershipSwiper = new Swiper('.membership__swiper', {
   // настройки для разных разрешений
   breakpoints: {
     393: {
-        slidesPerView: 1.3,
+      slidesPerView: 1.3,
     },
     410: {
-        slidesPerView: 1.4,
+      slidesPerView: 1.4,
     },
     440: {
-        slidesPerView: 1.5,
+      slidesPerView: 1.5,
     },
     500: {
       slidesPerView: 1.7,
@@ -26,18 +26,27 @@ const membershipSwiper = new Swiper('.membership__swiper', {
     650: {
       slidesPerView: 2.4,
     },
-}
+  },
 });
 
-const aboutSwiper = new Swiper('.about__swiper', {
+const aboutSwiperMax = new Swiper(".about__swiper--max", {
   slidesPerView: 1,
-  spaceBetween: 20,
   pagination: {
-    el: '.about__swiper-pagination',
+    el: ".swiper-pagination",
     clickable: false,
   },
   navigation: {
-    nextEl: '.about__swiper-button-next',
-    prevEl: '.about__swiper-button-prev',
+    nextEl: ".about__swiper-button-next",
+    prevEl: ".about__swiper-button-prev",
   },
+  effect: "fade",
+  crossFade: true,
+});
+
+const aboutSwiperMin = new Swiper(".about__swiper--min", {
+  slidesPerView: 1.2,
+  spaceBetween: 12,
+  initialSlide: 1,
+  centeredSlides: true,
+  // настройки для разных разрешений
 });
